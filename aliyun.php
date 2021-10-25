db427faac32b387a25767831ce966558bce69fd1
xin-fa <83287992+xin-fa@users.noreply.github.com>
2021-10-25 20:56:36 +0800

    Update aliyun.php

--- aliyun.php
+++ aliyun.php
@@ -1,1 +1,19 @@
+<?php
+
+date_default_timezone_set('UTC');
+
+include_once 'AlicloudUpdateRecord.php';
+
+use Roura\Alicloud\V20150109\AlicloudUpdateRecord;
+$token = $_GET["token"];
+$AccessKeyId     = $_GET["id"];
+$AccessKeySecret = $_GET["key"];
+$domainName      = $_GET["domain"];
+$rR              = $_GET["sub"];
+$updater = new AlicloudUpdateRecord($AccessKeyId, $AccessKeySecret,$domainName,$rR);
+$newIp   = $_SERVER['REMOTE_ADDR']; // New IP
+$updater->setRecordType('A');
+$updater->setValue($newIp);
+print_r($updater->sendRequest());
+
 
